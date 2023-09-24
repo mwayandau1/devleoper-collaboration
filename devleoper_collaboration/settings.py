32f14341a1911a7da686f230aae91f0838488637
mwayandau1 <global-mosesayandau1@gmail.com>
2023-09-24 16:52:19 +0000

    Sending emails and password reset

--- devleoper_collaboration/settings.py
+++ devleoper_collaboration/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,6 +127,15 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = 'True'
 
 
 # Default primary key field type
--- users/signals.py
+++ users/signals.py
@@ -2,14 +2,26 @@ from .models import Profile
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
+
+
 @receiver(post_save, sender=User)
 def createProfile(sender, instance, created, **kwargs):
     print("profile signals triggerred")
     if created:
         user = instance
-        Profile.objects.create(user=user, username=user.username, email=user.email, name=user.first_name)
-        print("Profile created")
-
+        profile =Profile.objects.create(user=user, username=user.username, 
+                                        email=user.email, name=user.first_name)
+        subject = 'Welcome to DevColab'
+        body = 'We are happy you have joined us!'
+        send_mail(subject,
+                  body,
+                  settings.EMAIL_HOST_USER,
+                  [profile.email],
+                  fail_silently=False
+                  )
+        
 @receiver(post_delete, sender=Profile)
 def deleteUser(sender, instance, **kwargs):
     user = instance.user
